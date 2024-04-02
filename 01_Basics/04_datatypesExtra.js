d76a9b121ae45f8a017f65e659f7f50245defd0e
Shubham Khanra <74144054+ShubhamKhanra@users.noreply.github.com>
2024-04-02 11:12:03 +0530

    Update 04_datatypesExtra.js

--- 01_Basics/04_datatypesExtra.js
+++ 01_Basics/04_datatypesExtra.js
@@ -23,3 +23,23 @@ console.log(myFun)
 console.log(typeof myFun)//type will be the -> function
 console.log(typeof arr)
 console.log(typeof a)
+
+
+//There are two types of memory - Stack and Heap. Primitive datatypes are stored in Stack Memory and Non Primitive datatypes are stored in Heap memory.
+//Stack Memory always provides a copy of the variable and Heap memory provides the reference. 
+
+let tiger = "wild"
+let cat = tiger
+cat = "Domestic"
+console.log(cat)
+console.log(tiger)
+//The value of the cat does not affect the value of the tiger because those variables are stored in the Stack memory.
+
+
+let obj = {
+    animal : "Tiger",
+    colour : "Yellow"
+}
+let obj1 = obj
+obj1.colour = "Black"
+console.log(obj.colour)// output will be Black because if I change the value of the variable once then it will affect all the values.
