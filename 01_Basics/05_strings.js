48ad5ec469c3c736d8d34a576d8be871e98a8437
Shubham Khanra <74144054+ShubhamKhanra@users.noreply.github.com>
2024-04-02 17:39:01 +0530

    Update 05_strings.js

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -7,3 +7,23 @@ console.log(strName.__proto__);
 console.log(strName.length)
 console.log(strName[3])
 console.log(strName.__proto__);
+console.log(strName.charAt(2));
+console.log(strName.indexOf('t'));
+
+const newString = strName.substring(0, 4)
+console.log(newString);//  Output : Deba
+
+const anotherString = strName.slice(-8, 4)
+console.log(anotherString);//Output : ba
+
+const newStringOne = "   shubham    "
+console.log(newStringOne);
+console.log(newStringOne.trim());//"shubham"
+
+const url = "https://shubham.com/shubham%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))// false
+
+console.log(url.split('/'));// Output : [ 'https:', '', 'shubham.com', 'shubham%20khanra' ]
