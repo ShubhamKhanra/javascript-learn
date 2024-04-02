56f12a0b765e01ffa2d02aabe46744a4ce618401
Shubham Khanra <74144054+ShubhamKhanra@users.noreply.github.com>
2024-04-02 18:10:50 +0530

    Update 07_date_time.js

--- 01_Basics/07_date_time.js
+++ 01_Basics/07_date_time.js
@@ -4,12 +4,12 @@ console.log(myTime.toDateString());
 console.log(myTime.toISOString());
 console.log(myTime.toLocaleString());
 console.log(myTime.toJSON());
-console.log(typeof myTime);
+console.log(typeof myTime);// Output : Object
 console.log(myTime);
 
 console.log("\t");
 let createDate = new Date("01-05-1999");
-let timeStamp = Date.now()
+let timeStamp = Date.now()// melisecond value from 1970
 console.log(Math.floor(createDate.getTime()/1000))
 console.log(Math.floor(timeStamp/1000))
 
