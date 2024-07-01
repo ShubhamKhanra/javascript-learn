d240e9bb0336d0986b96aca92befd7abf3ab938f
Shubham Khanra <74144054+ShubhamKhanra@users.noreply.github.com>
2024-07-01 09:10:50 +0530

    Update 5_IIFE.js

--- 03_Basics/5_IIFE.js
+++ 03_Basics/5_IIFE.js
@@ -1,10 +1,10 @@
 // Immediately Invoked Function Expression(IIFE)
 //IIFE use to Avoid pollution from the global scope.
-(function ciem(a,b){
+(function ciem(a,b){        \\named IIFE
     return console.log(`${b} will come today and ${a} came yesterday`)
 })("Rupam","Snigdhajeet");
 
-((a,b) => console.log(a + b))(5,9);// we can use arrow function
+((a,b) => console.log(a + b))(5,9);// we can use arrow function \\unnamed IIFE
 
 ((...ball) => {
     //for(i=0;i<ball.length;i++)
